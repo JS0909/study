6e0384f49bbf7871df6d9cac7013f455fe170307
JS0909 <108262555+JS0909@users.noreply.github.com>
2022-06-29 09:48:56 +0900

    Add files via upload

--- study/keras/keras03_batch.py
+++ study/keras/keras03_batch.py
@@ -1,3 +1,13 @@
+# naming rule
+# kaggle_bike -> python
+# kaggleBike -> Java (camel case)
+
+# batch : 분할해서 데이터 넣기
+
+# hyper parameter tuning
+# 노드 갯수, 레이어 깊이, 훈련 횟수, 분할
+
+
 # 1. 데이터
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -8,19 +18,17 @@ y = np.array([1, 2, 3, 4, 5])
 
 # 2. 모델구성 
 model = Sequential()
-model.add(Dense(80, input_dim=1))
-model.add(Dense(50))
-model.add(Dense(90))
-model.add(Dense(80))
+model.add(Dense(70, input_dim=1))
+model.add(Dense(60))
 model.add(Dense(70))
-model.add(Dense(90))
-model.add(Dense(80))
+model.add(Dense(60))
 model.add(Dense(70))
+model.add(Dense(60))
 model.add(Dense(1))
 
 # 3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x, y, epochs=50)
+model.fit(x, y, epochs=1, batch_size=1)
 
 
 # 4. 평가, 예측
@@ -30,14 +38,14 @@ print('loss : ', loss)
 result = model.predict([6])
 print('[6]의 예측값 : ', result)
 
-# 결과치 : 5.9909673
-# model.add(Dense(80, input_dim=1))
+
+#model.add(Dense(80, input_dim=1))
 # model.add(Dense(50))
 # model.add(Dense(90))
 # model.add(Dense(80))
 # model.add(Dense(70))
-# model.add(Dense(90))
 # model.add(Dense(80))
 # model.add(Dense(70))
 # model.add(Dense(1))
-# epochs = 50
+# epochs=1, batch_size=1
+# 결과치 : 5.9506383
