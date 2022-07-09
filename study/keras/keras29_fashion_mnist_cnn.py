69ed9c1b5cb1a85117feea03f517d52537dafcf8
JS0909 <108262555+JS0909@users.noreply.github.com>
2022-07-09 20:40:23 +0900

    Add files via upload

--- study/keras/keras29_fashion_mnist_cnn.py
+++ study/keras/keras29_fashion_mnist_cnn.py
@@ -10,11 +10,15 @@ from tensorflow.keras.utils import to_categorical
 # 1. 데이터
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
+
 print(x_train.shape, y_train.shape) # (60000, 28, 28) (60000,)
 print(x_test.shape, y_test.shape) # (10000, 28, 28) (10000,)
 
-x_train = x_train.reshape
-x_test = x_test.reshape
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1)
+
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
 
 print(np.unique(y_train, return_counts=True)) 
 
@@ -25,15 +29,15 @@ y_test=to_categorical(y_test)
 
 # 2. 모델구성
 model = Sequential()
-model.add(Conv2D(filters=64, kernel_size=(3,3), strides=1, padding='same', input_shape=(32, 32, 3)))
+model.add(Conv2D(filters=64, kernel_size=(3,3), strides=1, padding='same', input_shape=(28, 28, 1)))
 model.add(MaxPool2D())
-model.add(Conv2D(9, (2,2),padding='valid', activation='relu'))
-model.add(Conv2D(8, (3,3),padding='same', activation='relu'))
-model.add(Conv2D(10, (2,2),padding='valid', activation='relu'))
+model.add(Conv2D(32, (2,2),padding='valid'))
+model.add(Conv2D(32, (3,3),padding='same', activation='relu'))
+model.add(Conv2D(16, (2,2),padding='valid', activation='relu'))
 model.add(Flatten())
 model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(32))
+model.add(Dense(10, activation='sigmoid'))
 
 # 3. 컴파일, 훈련
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -49,3 +53,5 @@ acc_sc = accuracy_score(y_test, y_predict)
 print('loss : ', loss)
 print('acc스코어 : ', acc_sc)
 
+# loss :  [0.3210838735103607, 0.8909000158309937]
+# acc스코어 :  0.8905
